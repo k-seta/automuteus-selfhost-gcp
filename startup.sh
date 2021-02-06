df4ca77efec9f10d39554a8778c0ab69b12aa80d
k-seta <swordh16@gmail.com>
2021-02-06 11:27:11 +0900

    tee も上手く動作していないので削除

--- startup.sh
+++ startup.sh
@@ -20,4 +20,4 @@ docker run --rm \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v "$PWD:$PWD" \
     -w="$PWD" \
-    docker/compose:1.24.0 up 2>&1 | tee /automuteus.log
+    docker/compose:1.24.0 up
