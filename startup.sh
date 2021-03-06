d942743a3f60784e484f8f5cf8b1275868fb79bf
k-seta <swordh16@gmail.com>
2021-03-06 18:43:30 +0900

    factorio-dockerを導入

--- startup.sh
+++ startup.sh
@@ -3,6 +3,7 @@
 mkdir -p /home
 cd /home
 
+# AutoMuteUs
 git clone https://github.com/denverquane/automuteus.git
 cd automuteus
 
@@ -16,8 +17,20 @@ sed -i -e "s/GALACTUS_HOST=/GALACTUS_HOST=http:\/\/${EXTERNAL_IP}/g" .env
 sed -i -e 's/GALACTUS_EXTERNAL_PORT=/GALACTUS_EXTERNAL_PORT=80/g' .env
 sed -i -e "s/DISCORD_BOT_TOKEN=/DISCORD_BOT_TOKEN=${DISCORD_BOT_TOKEN}/g" .env
 
-docker run -d --rm \
+docker run -d \
+    --rm \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v "$PWD:$PWD" \
     -w="$PWD" \
     docker/compose:1.24.0 up
+
+# factorio
+mkdir -p /home/factorio
+chown 845:845 /home/factorio
+docker run -d \
+    -p 34197:34197/udp \
+    -p 27015:27015/tcp \
+    -v /home/factorio:/factorio \
+    --name factorio \
+    --restart=always \
+    factoriotools/factorio
